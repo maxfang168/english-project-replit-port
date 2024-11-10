74a34cf102c7b6d57a33a76d097ab909d197acca
maxfang168 <159708634+maxfang168@users.noreply.github.com>
2024-11-10 14:15:49 -0500

    Adjusted html files and script

--- mathscript.js
+++ mathscript.js
@@ -1,14 +1,13 @@
-
-const continueButton = document.getElementById('continueButton');
-pletters.classList.remove('paragraph');
-pletters.classList.add('paragraph-wide');
+setTimeout(() => {
+  transition(1)
+}, 1000);
 function transition(num) {
   if (num == 1) {
     document.body.classList.add('light-red');
-    
-    
+
+
     setTimeout(() => {
-      
+
       document.getElementById('pletters').textContent = 'During 2-3rd grade, I absolutely despised math. \nWhenever I was forced by my mom to work on math with her, I felt like I could do things much more productive during that time, such as reading a book.';
       setTimeout(() => {
         document.getElementById('pletters').textContent = 'During 2-3rd grade, I absolutely despised math. \nWhenever I was forced by my mom to work on math with her, I felt like I could do things much more productive during that time, such as reading a book.';
@@ -25,21 +24,19 @@ function transition(num) {
                   setTimeout(() => {
                     document.getElementById('pletters').textContent = 'During 2-3rd grade, I absolutely despised math. \nWhenever I was forced by my mom to work on math with her, I felt like I could do things much more productive during that time, such as reading a book. I also felt that math was a pointless waste of my time. I was always tired of it. I began to pout every time I saw a math problem, and I would always pout when my mom would force me to do math. Eventually, I went for my typical strategy. I would act depressed and unhappy. I would also pretend to fall asleep during lessons.';
                     setTimeout(() => {
-                      document.getElementById('pletters').textContent = 'During 2-3rd grade, I absolutely despised math. \nWhenever I was forced by my mom to work on math with her, I felt like I could do things much more productive during that time, such as reading a book. I also felt that math was a pointless waste of my time. I was always tired of it. I began to pout every time I saw a math problem, and I would always pout when my mom would force me to do math. Eventually, I went for my typical strategy. I would act depressed and unhappy. I would also pretend to fall asleep during lessons. My younger-self\'s reasoning is still partially unknown, but my prevailing theory is that I wanted my mom to see that I was getting depressed, and stop teaching me, for my wellbeing.';
+                      document.getElementById('ple  tters').textContent = 'During 2-3rd grade, I absolutely despised math. \nWhenever I was forced by my mom to work on math with her, I felt like I could do things much more productive during that time, such as reading a book. I also felt that math was a pointless waste of my time. I was always tired of it. I began to pout every time I saw a math problem, and I would always pout when my mom would force me to do math. Eventually, I went for my typical strategy. I would act depressed and unhappy. I would also pretend to fall asleep during lessons. My younger-self\'s reasoning is still partially unknown, but my prevailing theory is that I wanted my mom to see that I was getting depressed, and stop teaching me, for my wellbeing.';
                       setTimeout(() => {
                         document.getElementById('pletters').textContent = 'During 2-3rd grade, I absolutely despised math. \nWhenever I was forced by my mom to work on math with her, I felt like I could do things much more productive during that time, such as reading a book. I also felt that math was a pointless waste of my time. I was always tired of it. I began to pout every time I saw a math problem, and I would always pout when my mom would force me to do math. Eventually, I went for my typical strategy. I would act depressed and unhappy. I would also pretend to fall asleep during lessons. My younger-self\'s reasoning is still partially unknown, but my prevailing theory is that I wanted my mom to see that I was getting depressed, and stop teaching me, for my wellbeing. Now, I realise how selfish I was, and how I wasted my mom\'s time.';
-                        }, 3);
-                      }, 8);
-                    }, 53);
-                  }, 243);
-                }, 343);
-              }, 603);
-            }, 1043);
-          }, 1223);
-        }, 1153);
+                      }, 3);
+                    }, 8);
+                  }, 53);
+                }, 243);
+              }, 343); 
+            }, 603);
+          }, 1043);
+        }, 1223);
+      }, 1153);
     }, 3000);
-    document.pletters.classList.remove('paragraph-wide');
-    document.pletters.classList.add('paragraph');
   }
   if (num == 2) {
     document.body.classList.remove('light-red');
